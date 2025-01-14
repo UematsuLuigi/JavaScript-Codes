4d835d879ca7e614c6bbf0397a8173f4d1326187
Luigi Uematsu <126525709+UematsuLuigi@users.noreply.github.com>
2025-01-14 10:56:14 -0300

    Projeto pokedex 1.0

---
+++ pokedex1.js
@@ -0,0 +1,273 @@
+cartaEscolhida = `002`;
+
+const pokemon1 = { 
+    nome: 'Bulbasaur', 
+    tipo: 'grama',
+    nIdentificao: '001',
+    habilidades: 'Vine Whipe',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon2 = { 
+    nome: 'Ivysaur', 
+    tipo: 'grama',
+    nIdentificao: '002',
+    habilidades: 'Razor Leaf',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon3 = { 
+    nome: 'Venusaur', 
+    tipo: 'grama',
+    nIdentificao: '003',
+    habilidades: 'Mega Drain',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon4 = { 
+    nome: 'Venusaur EX', 
+    tipo: 'grama',
+    nIdentificao: '004',
+    habilidades: 'Razor Lead and Glant Bloom',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon5 = { 
+    nome: 'Caterpie', 
+    tipo: 'grama',
+    nIdentificao: '005',
+    habilidades: 'Find a Friend',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon6 = { 
+    nome: 'Metapod', 
+    tipo: 'grama',
+    nIdentificao: '006',
+    habilidades: 'Bug Bite',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon7 = { 
+    nome: 'Butterfree', 
+    tipo: 'grama',
+    nIdentificao: '007',
+    habilidades: 'Powder Heal and Gust',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon8 = { 
+    nome: 'Weedle', 
+    tipo: 'grama',
+    nIdentificao: '008',
+    habilidades: 'Sting',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon9 = { 
+    nome: 'Kakuna', 
+    tipo: 'grama',
+    nIdentificao: '009',
+    habilidades: 'Bug Bite',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon10 = { 
+    nome: 'Beedrill', 
+    tipo: 'grama',
+    nIdentificao: '010',
+    habilidades: 'Sharp Sting',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon11 = { 
+    nome: 'Oddish', 
+    tipo: 'grama',
+    nIdentificao: '011',
+    habilidades: 'Ram',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon12 = { 
+    nome: 'Gloom', 
+    tipo: 'grama',
+    nIdentificao: '012',
+    habilidades: 'Drool',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon13 = { 
+    nome: 'Vileplume', 
+    tipo: 'grama',
+    nIdentificao: '013',
+    habilidades: 'Soothing Scent',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon14 = { 
+    nome: 'Paras', 
+    tipo: 'grama',
+    nIdentificao: '014',
+    habilidades: 'Scratch',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon15 = { 
+    nome: 'Parasect', 
+    tipo: 'grama',
+    nIdentificao: '015',
+    habilidades: 'Slash',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon16 = { 
+    nome: 'Venonat', 
+    tipo: 'grama',
+    nIdentificao: '016',
+    habilidades: 'Tackle',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon17 = { 
+    nome: 'Venomoth', 
+    tipo: 'grama',
+    nIdentificao: '017',
+    habilidades: 'Poison Powder',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon18 = { 
+    nome: 'Bellsprout', 
+    tipo: 'grama',
+    nIdentificao: '018',
+    habilidades: 'Vine Whipe',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon19 = { 
+    nome: 'Weepinbell', 
+    tipo: 'grama',
+    nIdentificao: '019',
+    habilidades: 'Razor Leaf',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon20 = { 
+    nome: 'Victreebel', 
+    tipo: 'grama',
+    nIdentificao: '020',
+    habilidades: 'Fragrance Trap and Vine Whipe',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon21 = { 
+    nome: 'Exeggcute', 
+    tipo: 'grama',
+    nIdentificao: '021',
+    habilidades: 'Seed Bomb',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon22 = { 
+    nome: 'Exeggutor', 
+    tipo: 'grama',
+    nIdentificao: '022',
+    habilidades: 'Stomp',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon23 = { 
+    nome: 'Exeggutor EX', 
+    tipo: 'grama',
+    nIdentificao: '023',
+    habilidades: 'Tropical Swing',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon24 = { 
+    nome: 'Tangela', 
+    tipo: 'grama',
+    nIdentificao: '024',
+    habilidades: 'Absorb',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon25 = { 
+    nome: 'Scyther', 
+    tipo: 'grama',
+    nIdentificao: '025',
+    habilidades: 'Sharp Scythe',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon26 = { 
+    nome: 'Pinsir', 
+    tipo: 'grama',
+    nIdentificao: '026',
+    habilidades: 'Double Horn',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon27 = { 
+    nome: 'Cottonee', 
+    tipo: 'grama',
+    nIdentificao: '027',
+    habilidades: 'Attach',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon28 = { 
+    nome: 'Whimsicott', 
+    tipo: 'grama',
+    nIdentificao: '028',
+    habilidades: 'Rolling Tackle',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon29 = { 
+    nome: 'Petilil', 
+    tipo: 'grama',
+    nIdentificao: '029',
+    habilidades: 'Blot',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon30 = { 
+    nome: 'Lilligant', 
+    tipo: 'grama',
+    nIdentificao: '030',
+    habilidades: 'Leaf Supply',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon31 = { 
+    nome: 'Skiddo', 
+    tipo: 'grama',
+    nIdentificao: '031',
+    habilidades: 'Surprise Attack',
+    colecao: 'Dominação Genética'
+};
+
+const pokemon32 = { 
+    nome: 'Gogoat', 
+    tipo: 'grama',
+    nIdentificao: '032',
+    habilidades: 'Razor Leaf',
+    colecao: 'Dominação Genética'
+};
+
+const listaTeste = [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6, pokemon7, pokemon8, pokemon9, pokemon10, pokemon11, pokemon12, pokemon13, pokemon14, pokemon15, pokemon16, pokemon17, pokemon18, pokemon19, pokemon20, pokemon21, pokemon22, pokemon23, pokemon24, pokemon25, pokemon26, pokemon27, pokemon28, pokemon29, pokemon30, pokemon31, pokemon32];
+
+function buscarCarta(codigo) {
+    const posicao = parseInt(codigo, 10) - 1;
+
+    if (posicao >= 0 && posicao < listaTeste.length) {
+        return listaTeste[posicao];
+    } else {
+        return "Código inválido";
+    }
+}
+
+const resultado = buscarCarta(cartaEscolhida);
+
+console.log(`A carta ${resultado.nIdentificao} é o pokemon ${resultado.nome}, sendo do tipo ${resultado.tipo} com o(s) ataque(s) ${resultado.habilidades} e pertencendo a coleção ${resultado.colecao}`);
